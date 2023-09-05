70ec81351fbe81d4a5fddc0c12dc135d24fc0f7c
DaLae37 <dalae37@gmail.com>
2023-09-05 17:19:19 +0900

    Add app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__) 
+api = Api(app)
+
+@api.route('/connect')
+class Connect(Resource) :
+    def get(self) :
+        return {"connect" : "success"}
+
+@api.route('/usermaps') 
+class UserMaps(Resource):
+    def get(self):
+        return {"connect" : "success"}
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=3851)
